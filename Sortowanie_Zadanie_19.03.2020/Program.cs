29d6b5bad80645d4d94d93d5d540a7c808e9b73c
TheHund1 <matuszak.piotr@outlook.com>
2021-03-20 16:04:57 +0100

    Initial commit

---
+++ Sortowanie_Zadanie_19.03.2020/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Sortowanie_Zadanie_19._03._2020
+{
+    class Program
+    {
+        static void WeryfikacjaInputu(ref int x)
+        {
+            bool czyPrawidłowo = true;
+            while (czyPrawidłowo)
+            {
+                try
+                {
+                    x = Convert.ToInt32(Console.ReadLine());
+                    czyPrawidłowo = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Wartość nie może być pusta.");
+                    Console.WriteLine("Wprowadź wartość ponownie.");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int rozmiar = 8;
+            int[] tab = new int[rozmiar];
+            bool czyWybórTabeli = true;
+            while (czyWybórTabeli)
+            {
+                Console.WriteLine("Wybierz opcje: \nWprowadzenie tabeli samemu[1] \nUżycie predefiniowanej tabeli[2]");
+                int wybórTabeli=1;
+                WeryfikacjaInputu(ref wybórTabeli);
+                switch (wybórTabeli)
+                {
+                    case 1:
+                        Console.WriteLine("Wybrałeś opcję wprowadzenie tabeli samemu.");
+                        Console.Write("Wprowadź rozmiar tabeli: ");
+                        WeryfikacjaInputu(ref rozmiar);
+                        tab = new int[rozmiar];
+                        for (int i = 0; i < tab.Length; i++)
+                        {
+                            WeryfikacjaInputu(ref tab[i]);
+                        }
+                        foreach (var a in tab)
+                        {
+                            Console.Write(" " + a);
+                        }
+                        Console.WriteLine();
+                        czyWybórTabeli = false;
+                        break;
+                    case 2:
+                        Console.WriteLine("Wybrałeś opcję użycie predefiniowanej tabeli.");
+                        Console.WriteLine("Oto Twoja tabela:");
+                        tab = new int[] { 9, 1, 2, 4, 5, 7, 8, 6, 3 };
+                        foreach (var a in tab)
+                        {
+                            Console.Write(" " + a);
+                        }
+                        Console.WriteLine();
+                        czyWybórTabeli = false;
+                        break;
+                    default:
+                        Console.WriteLine("Błędna wartość. Wybierz ponownie.");
+                        break;
+                }
+            }
+            bool czyWybórSortowania = true;
+            while (czyWybórSortowania)
+            {
+                Console.WriteLine("Wybierz opcje: \nQuickSort[1] \nQuickSort z komentarzem[2]");
+                int wybórSortowania = 1;
+                WeryfikacjaInputu(ref wybórSortowania);
+                switch (wybórSortowania)
+                {
+                    case 1:
+                        Console.WriteLine("Wybrałeś opcję QuickSort.");
+                        QuickSort.Sortowanie(tab);
+                        czyWybórSortowania = false;
+                        break;
+                    case 2:
+                        Console.WriteLine("Wybrałeś opcję QuickSort.");
+                        QuickSort.SortowanieZKomentarzem(tab);
+                        czyWybórSortowania = false;
+                        break;
+                    default:
+                        Console.WriteLine("Błędna wartość. Wybierz ponownie.");
+                        break;
+                }
+                Console.WriteLine("Posortowana tabela:");
+                foreach (var a in tab)
+                {
+                    Console.Write(" " + a);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Sortowanie_Zadanie_19.03.2020/QuickSort.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sortowanie_Zadanie_19._03._2020
+{
+    class QuickSort
+    {
+        public static int[] Sortowanie(int[] tablica)
+        {
+            void Quick_Sort(int[] arr, int left, int right)
+            {
+                if (left < right)
+                {
+                    int pivot = Partition(arr, left, right);
+
+                    if (pivot > 1)
+                    {
+                        Quick_Sort(arr, left, pivot - 1);
+                    }
+                    if (pivot + 1 < right)
+                    {
+                        Quick_Sort(arr, pivot + 1, right);
+                    }
+                }
+            }
+
+            static int Partition(int[] tab, int left, int right)
+            {
+                int temp;
+                int pivot = tab[right];
+                int i = left - 1;
+                for (int j = left; j <= right - 1; j++)
+                {
+                    if (tab[j] <= pivot)
+                    {
+                        i++;
+                        temp = tab[i];
+                        tab[i] = tab[j];
+                        tab[j] = temp;
+                    }
+                }
+
+                temp = tab[i + 1];
+                tab[i + 1] = tab[right];
+                tab[right] = temp;
+                return i + 1;
+            }
+            Quick_Sort(tablica, 0, tablica.Length - 1);
+            return tablica;
+        }
+        public static int[] SortowanieZKomentarzem(int[] tablica)
+        {
+            Console.WriteLine("LEGENDA:");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("To jest kolor pivota.");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("To jest kolor elementu zamienianego z pivotem.");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("To jest kolor w momencie kiedy pivot nie jest zamieniany z żadnym elementem.");
+            Console.ResetColor();
+            void Quick_Sort1(int[] arr, int left, int right)
+            {
+                if (left < right)
+                {
+                    int pivot = Partition1(arr, left, right);
+
+                    if (pivot > 1)
+                    {
+                        Quick_Sort1(arr, left, pivot - 1);
+                    }
+                    if (pivot + 1 < right)
+                    {
+                        Quick_Sort1(arr, pivot + 1, right);
+                    }
+                }
+            }
+
+            static int Partition1(int[] tab, int left, int right)
+            {
+                int temp;
+                int pivot = tab[right];
+                int i = left - 1;
+                Console.WriteLine("\n");
+                Console.WriteLine("Pivot jest równy {0}", pivot);
+                Console.WriteLine("\n");
+                for (int j = left; j <= right - 1; j++)
+                {
+                    if (tab[j] <= pivot)
+                    {
+                        i++;
+                        temp = tab[i];
+                        tab[i] = tab[j];
+                        tab[j] = temp;
+                    }
+                }
+
+                temp = tab[i + 1];
+                tab[i + 1] = tab[right];
+                tab[right] = temp;
+                for (int a = 0; a < tab.Length; a++)
+                {
+                    if (tab[a] == tab[i + 1] && tab[a] != tab[right])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(" " + tab[a]);
+                        Console.ResetColor();
+                    }
+                    if (tab[a] == tab[right] && tab[a] != tab[i + 1])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(" " + tab[a]);
+                        Console.ResetColor();
+                    }
+                    if (tab[a] == tab[right] && tab[a] == tab[i + 1])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.Write(" " + tab[a]);
+                        Console.ResetColor();
+                    }
+                    if (tab[a] != tab[i + 1] && tab[a] != tab[right])
+                    {
+                        Console.Write(" " + tab[a]);
+                    }
+                }
+                return i + 1;
+            }
+            Quick_Sort1(tablica, 0, tablica.Length - 1);
+            return tablica;
+        }
+    }
+}
